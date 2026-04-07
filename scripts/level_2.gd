f019c4f01c9c01cbff566abb118cad3fff5cca31
Libor Výleta <136346568+liborvyleta@users.noreply.github.com>
2026-04-07 18:58:08 +0200

    Add LevelEnd area and transition to Level 2; update project settings

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -1,16 +1,14 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$"/root/GameManager/GUI".show()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
 func _on_level_end_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+		get_tree().change_scene_to_file("res://scenes/level_3.tscn")
